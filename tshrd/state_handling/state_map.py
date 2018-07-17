bae4e4ddae5a1521db6a158cef59186ee5152b8d
Dan Alexander <lxndrdagreat@gmail.com>
2018-07-17 16:49:54 -0400

    added map scrolling

--- tshrd/state_handling/state_map.py
+++ tshrd/state_handling/state_map.py
@@ -17,8 +17,6 @@ def state(game: GameData, root_console: tdl.Console) -> GameState:
     total_height = (room_size + (spacing * 2)) * the_map.size[1]
 
     map_console = tdl.Console(total_width, total_height)
-    # map_console.set_colors(fg=(255, 255, 255), bg=(0, 0, 100))
-    map_console.clear()
 
     legend_scheme = {
         'monster': (180, 0, 0),
@@ -28,44 +26,6 @@ def state(game: GameData, root_console: tdl.Console) -> GameState:
         'stairs': (0, 200, 235),
         'empty': (255, 255, 255)
     }
-
-    # draw the rooms
-    map_center_x = 0
-    map_center_y = 0
-    for room in [room for room in the_map.rooms if room.discovered]:
-        x = room.x * (room_size + spacing) + spacing
-        y = room.y * (room_size + spacing) + spacing
-
-        for xx in range(x, x + room_size):
-            for yy in range(y, y + room_size):
-                t = '.'
-                if xx == x or xx == x + room_size - 1 or yy == y or yy == y + room_size - 1:
-                    t = '#'
-                if room.north and yy == y and xx == x + 2:
-                    t = '.'
-                if room.south and yy == y + room_size - 1 and xx == x + 2:
-                    t = '.'
-                if room.east and yy == y + 2 and xx == x + room_size - 1:
-                    t = '.'
-                if room.west and yy == y + 2 and xx == x:
-                    t = '.'
-
-                color = (255, 255, 255)
-
-                if room.encounter and room.encounter in legend_scheme:
-                    color = legend_scheme[room.encounter]
-
-                map_console.draw_char(xx, yy, t, bg=None, fg=color)
-
-        if room == game.current_room:
-            map_center_x = x + int(room_size / 2)
-            map_center_y = y + int(room_size / 2)
-            map_console.draw_char(x + int(room_size / 2), y + int(room_size / 2), game.the_player.tile, fg=(255, 255, 255))
-
-    src_x = max(map_center_x - 15, 0)
-    src_y = max(map_center_y - 15, 0)
-    root_console.draw_rect(0, 0, 30, 30, None, bg=(50, 50, 50))
-    root_console.blit(map_console, 0, 0, 30, 30, src_x, src_y, 1.0, 0.0)
 
     # legend
     x = root_console.width - 20
@@ -87,10 +47,68 @@ def state(game: GameData, root_console: tdl.Console) -> GameState:
     # bottom bar
     root_console.draw_rect(0, root_console.height - 3, root_console.width, 3, None, bg=(200, 200, 200))
     root_console.draw_str(1, root_console.height - 2, '[ESC] back', fg=(0, 0, 0), bg=None)
-    # root_console.draw_str(20, root_console.height - 2, '[Arrow Keys] scroll map', fg=(0, 0, 0), bg=None)
+    root_console.draw_str(15, root_console.height - 2, '[Arrow Keys] scroll map', fg=(0, 0, 0), bg=None)
+    root_console.draw_str(30, root_console.height - 2, '[Enter] center map', fg=(0, 0, 0), bg=None)
 
-    tdl.flush()
+    map_center_x = game.current_room.x * (room_size + spacing) + spacing
+    map_center_y = game.current_room.y * (room_size + spacing) + spacing
 
-    wait_for_keys(['ESCAPE', 'SPACE', 'ENTER', 'm'])
+    # draw the rooms
+    while True:
+        map_console.clear()
+        for room in [room for room in the_map.rooms if room.discovered]:
+            x = room.x * (room_size + spacing) + spacing
+            y = room.y * (room_size + spacing) + spacing
+
+            for xx in range(x, x + room_size):
+                for yy in range(y, y + room_size):
+                    t = '.'
+                    if xx == x or xx == x + room_size - 1 or yy == y or yy == y + room_size - 1:
+                        t = '#'
+                    if room.north and yy == y and xx == x + 2:
+                        t = '.'
+                    if room.south and yy == y + room_size - 1 and xx == x + 2:
+                        t = '.'
+                    if room.east and yy == y + 2 and xx == x + room_size - 1:
+                        t = '.'
+                    if room.west and yy == y + 2 and xx == x:
+                        t = '.'
+
+                    color = (255, 255, 255)
+
+                    if room.encounter and room.encounter in legend_scheme:
+                        color = legend_scheme[room.encounter]
+
+                    map_console.draw_char(xx, yy, t, bg=None, fg=color)
+
+            if room == game.current_room:
+                map_console.draw_char(x + int(room_size / 2), y + int(room_size / 2), game.the_player.tile, fg=(255, 255, 255))
+
+        src_x = max(map_center_x - 15, 0)
+        src_y = max(map_center_y - 15, 0)
+        root_console.draw_rect(0, 0, 30, 30, ' ', bg=(50, 50, 50))
+        root_console.blit(map_console, 0, 0, 30, 30, src_x, src_y, 1.0, 0.0)
+
+        tdl.flush()
+        response = wait_for_keys(['ESCAPE', 'SPACE', 'ENTER', 'm', 'UP', 'DOWN', 'LEFT', 'RIGHT'])
+        if response in ['ESCAPE', 'SPACE', 'm']:
+            break
+        # scroll the map
+        if response == 'UP':
+            if map_center_y > 0:
+                map_center_y -= 1
+        elif response == 'DOWN':
+            if map_center_y < total_height - 1:
+                map_center_y += 1
+        elif response == 'LEFT':
+            if map_center_x > 0:
+                map_center_x -= 1
+        elif response == 'RIGHT':
+            if map_center_x < total_width - 1:
+                map_center_x += 1
+        elif response == 'ENTER':
+            # recenter the map
+            map_center_x = game.current_room.x * (room_size + spacing) + spacing
+            map_center_y = game.current_room.y * (room_size + spacing) + spacing
 
     return GameState.ROOM
