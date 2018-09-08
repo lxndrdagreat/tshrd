ded06641076111e4e22adc6998e1e6120bf673da
Dan Alexander <lxndrdagreat@gmail.com>
2018-09-07 22:15:45 -0400

    simple trap that causes turn loss

--- tshrd/state_handling/state_encounter_trap.py
+++ tshrd/state_handling/state_encounter_trap.py
@@ -3,6 +3,10 @@ from tshrd.state_handling import GameData, GameState
 
 
 def state(game: GameData, root_console: tdl.Console) -> GameState:
-    print('You find a trap!')
+    if game.current_room.encountered:
+        return GameState.ROOM
+    game.advance_turn()
+    log_text = f'You encounter a trap. You lose 1 turn and consume food.'
+    game.log(log_text, (149, 108, 53))
     game.current_room.encountered = True
     return GameState.ROOM
